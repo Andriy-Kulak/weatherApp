f6a0e65daef1bd5db9a11ea9cc29ff2acb556b1a
Andriy <andkulak@gmail.com>
2015-12-15 00:27:26 -0500

    Added doc blocs, additional weather info , automated search function

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ weatherApp.service('cityService', function() {
 /**
  * Main page Controller
  */
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('homeController', ['$scope', '$location', 'cityService', function($scope, $location, cityService) {
 
 	$scope.city = cityService.city;
 
@@ -47,6 +47,11 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
 		cityService.city = $scope.city;
 	});
 
+	// Will submit form when user presses enter
+	$scope.submit = function() {
+		$location.path("/forecast");
+	};
+
 }]);
 
 /**
@@ -54,23 +59,34 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
  * $routeParams - allows you to retrieve the current set of route parameters.
  * $resource - factory which creates a resource object that lets you interact with RESTful server-side data sources.
  */
-weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService',
+	function($scope, $resource, $routeParams, cityService) {
 
 	$scope.city = cityService.city;
 
 	$scope.days = $routeParams.days || '2';
 
-	$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=072df5ee8e2deb7041c6b02409370a64", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+	//API Get request for forecast
+	$scope.weatherAPI =
+		$resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=072df5ee8e2deb7041c6b02409370a64",
+			{ callback: "JSON_CALLBACK" },
+			{ get: { method: "JSONP" }});
 
 	$scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
 
-
+	//converts API Kelvin to Fahrenheit
 	$scope.convertToFahrenheit = function(degK) {
 		return Math.round((1.8 * (degK - 273)) + 32);
 	};
 
+	//converts API date data to human readable day
 	$scope.convertToDate = function(dt) {
 		return new Date(dt * 1000);
 	};
+
+		//test
+		$scope.getDescrip = function(x) {
+			return x['description'];
+		}
 
 }]);
